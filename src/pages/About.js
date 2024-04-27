f8ea43a78b2fe804fe6cef3edc547c094383b836
Samantha <samanthavillafana8@gmail.com>
2024-04-27 16:11:05 -0400

    Complete lab

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "./Navbar";
+// About.js
+function About() {
+    return (
+      <>
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <h1>This is my about component!</h1>
+        </main>
+      </>
+    );
+  };
+  
+  export default About;
